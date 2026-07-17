d1d8adc374bd258ed1048b3bb78f593dd4f0a435
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    cleanup in SpeedscopeParserTestsJsonExtensions

--- tests/SkiaFlameGraph.Tests/SpeedscopeParserTestsJsonExtensions.cs
+++ tests/SkiaFlameGraph.Tests/SpeedscopeParserTestsJsonExtensions.cs
@@ -5,46 +5,72 @@ using SkiaFlameGraph.Core.Parsing;
 
 namespace SkiaFlameGraph.Tests;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extension methods for <see cref="SpeedscopeParserTests"/>.
+/// </summary>
 public static class SpeedscopeParserTestsJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = true,
-        NumberHandling = JsonNumberHandling.AllowReadingFromString
-    };
+	private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = true,
+		NumberHandling = JsonNumberHandling.AllowReadingFromString
+	};
 
-    public static string ToJson(this SpeedscopeParserTests value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-        return JsonSerializer.Serialize(value, _jsonSerializerOptions);
-    }
+	/// <summary>
+	/// Serializes a <see cref="SpeedscopeParserTests"/> instance to a JSON string.
+	/// </summary>
+	/// <param name="value">The instance to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the object.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+	public static string ToJson(this SpeedscopeParserTests value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
+		var options = indented
+			? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+			: _jsonSerializerOptions;
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    public static SpeedscopeParserTests? FromJson(string json)
-    {
-        ArgumentNullException.ThrowIfNull(json);
-        try
-        {
-            return JsonSerializer.Deserialize<SpeedscopeParserTests>(json, _jsonSerializerOptions);
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
-    }
+	/// <summary>
+	/// Deserializes a JSON string to a <see cref="SpeedscopeParserTests"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized object, or <see langword="null"/> if deserialization fails.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+	public static SpeedscopeParserTests? FromJson(string json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
+		try
+		{
+			return JsonSerializer.Deserialize<SpeedscopeParserTests>(json, _jsonSerializerOptions);
+		}
+		catch (JsonException)
+		{
+			return null;
+		}
+	}
 
-    public static bool TryFromJson(string json, out SpeedscopeParserTests? value)
-    {
-        ArgumentNullException.ThrowIfNull(json);
-        try
-        {
-            value = JsonSerializer.Deserialize<SpeedscopeParserTests>(json, _jsonSerializerOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
+	/// <summary>
+	/// Attempts to deserialize a JSON string to a <see cref="SpeedscopeParserTests"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized object if successful; otherwise, <see langword="null"/>.</param>
+	/// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+	public static bool TryFromJson(string json, out SpeedscopeParserTests? value)
+	{
+		ArgumentNullException.ThrowIfNull(json);
+		try
+		{
+			value = JsonSerializer.Deserialize<SpeedscopeParserTests>(json, _jsonSerializerOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			value = null;
+			return false;
+		}
+	}
 }
