9550e5297aa95f500f568056ddc804b4b09da169
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-19 10:42:15 +0000

    chore: apply workspace updates

---
+++ src/SkiaFlameGraph.Core/Rendering/TreemapRendererJsonExtensionsConstants.cs
@@ -0,0 +1,8 @@
+namespace SkiaFlameGraph.Core.Rendering;
+
+/// <summary>
+/// Contains constants used by <see cref="TreemapRendererJsonExtensions"/>.
+/// </summary>
+internal static class TreemapRendererJsonExtensionsConstants
+{
+}
