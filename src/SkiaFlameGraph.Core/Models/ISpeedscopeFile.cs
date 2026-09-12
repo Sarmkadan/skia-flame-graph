b519fe163372eb8377696598f2372bebaa0bb6e7
Your Name <you@example.com>
2026-09-12 19:13:44 +0000

    docs: document ISpeedscopeFile members

--- src/SkiaFlameGraph.Core/Models/ISpeedscopeFile.cs
+++ src/SkiaFlameGraph.Core/Models/ISpeedscopeFile.cs
@@ -5,9 +5,28 @@ namespace SkiaFlameGraph.Core.Models;
 /// </summary>
 public interface ISpeedscopeFile
 {
+    /// <summary>
+    /// Gets or sets the schema version.
+    /// </summary>
     string? Schema { get; set; }
+
+    /// <summary>
+    /// Gets or sets the shared data.
+    /// </summary>
     SharedData Shared { get; set; }
+
+    /// <summary>
+    /// Gets or sets the profiles.
+    /// </summary>
     List<Profile> Profiles { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the file.
+    /// </summary>
     string? Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the exporter.
+    /// </summary>
     string? Exporter { get; set; }
 }
