de09980e82afcd0d37b84a35ca94a211744d04bc
Your Name <you@example.com>
2026-09-12 23:34:20 +0000

    doc: add XML doc comments to IFlameGraphRenderer interface members

--- src/SkiaFlameGraph.Core/Rendering/IFlameGraphRenderer.cs
+++ src/SkiaFlameGraph.Core/Rendering/IFlameGraphRenderer.cs
@@ -5,6 +5,16 @@ namespace SkiaFlameGraph.Core.Rendering;
 
 public interface IFlameGraphRenderer
 {
+    /// <summary>
+    /// Renders the flame graph to a PNG file.
+    /// </summary>
+    /// <param name="root">The root node of the flame graph.</param>
+    /// <param name="path">The file path to save the PNG.</param>
     void RenderToPng(FlameNode root, string path);
+    /// <summary>
+    /// Renders the flame graph and returns an SKImage.
+    /// </summary>
+    /// <param name="root">The root node of the flame graph.</param>
+    /// <returns>The rendered flame graph as an SKImage.</returns>
     SKImage Render(FlameNode root);
 }
